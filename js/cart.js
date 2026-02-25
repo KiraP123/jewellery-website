6e0492a08e3c57a85ad4425db96c287158ccadf5
KiraP123 <prajapatikiran6764@gmail.com>
2026-02-25 19:32:40 +0530

    css

--- js/cart.js
+++ js/cart.js
@@ -117,11 +117,11 @@ function updateCartUI() {
                 </div>
 
                 <div class="row g-3 align-items-center">
-                    <div class="col-4 col-md-2 text-center">
-                           <img src="images/${item.image}" class="rounded border me-3" style="width:65px; height:65px; object-fit:contain;"></img>
-                             class="img-fluid rounded-3 border p-1" 
-                             style="max-height: 90px; width: 100%; object-fit: contain; background: #fdfdfd;">
-                    </div>
+                   <div class="col-4 col-md-2 text-center">
+                               <img src="images/${item.image}" 
+                              class="img-fluid rounded-3 border p-1" 
+                         style="max-height: 90px; width: 100%; object-fit: contain; background: #fdfdfd;">
+                   </div>
 
                     <div class="col-8 col-md-6">
                         <div class="d-flex flex-column h-100">
